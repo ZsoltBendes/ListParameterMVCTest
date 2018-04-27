a8478ed30917d46ded6794c021bf71d9876a4854
Zsolt Bendes <zsolt.bendes@attrecto.com>
2018-04-27 09:15:35 +0200

    Initial commit

---
+++ ListParameterTest/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListParameterTest.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get([FromQuery]List<int> ids)
+        {
+            foreach (var item in ids)
+            {
+                Debug.WriteLine(item.ToString());
+            }
+
+            return Ok();
+        }
+    }
+}
